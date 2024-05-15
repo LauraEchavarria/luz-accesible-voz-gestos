191b3b4d5891564cab1aa953eafd8b484f0bd75b
Laura Echavarria <72629300+LauraEchavarria@users.noreply.github.com>
2024-05-15 18:39:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
+# TEACHABLE MACHINE
 import streamlit as st
 import cv2
 import numpy as np
 #from PIL import Image
 from PIL import Image as Image, ImageOps as ImagOps
 from keras.models import load_model
+
+# VOICE CONTROL
+import os
+import streamlit as st
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
+from streamlit_bokeh_events import streamlit_bokeh_events
+from PIL import Image
+import time
+import glob
+import paho.mqtt.client as paho
+import json
+from gtts import gTTS
+from googletrans import Translator
+
+# TEACHABLE MACHINE
 
 model = load_model('keras_model.h5')
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
@@ -37,5 +54,76 @@ if img_file_buffer is not None:
       st.header('Arriba, con Probabilidad: '+str( prediction[0][1]))
     if prediction[0][2]>0.5:
       st.header('Derecha, con Probabilidad: '+str( prediction[0][2]))
+
+# VOICE CONTROL CODE
+
+def on_publish(client,userdata,result):   #create function for callback
+    print("el dato ha sido publicado \n")
+    pass
+
+def on_message(client, userdata, message):
+    global message_received
+    time.sleep(2)
+    message_received=str(message.payload.decode("utf-8"))
+    st.write(message_received)
+
+broker="157.230.214.127"
+port=1883
+client1= paho.Client("FLORENCIA")
+client1.on_message = on_message
+
+
+st.title("Interfaces Multimodales")
+st.subheader("CONTROL POR VOZ")
+
+image = Image.open('voice_ctrl.jpg')
+
+st.image(image, width=200)
+
+
+st.write("Toca el Botón y habla ")
+
+stt_button = Button(label=" Inicio ", width=200)
+
+stt_button.js_on_event("button_click", CustomJS(code="""
+    var recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
+ 
+    recognition.onresult = function (e) {
+        var value = "";
+        for (var i = e.resultIndex; i < e.results.length; ++i) {
+            if (e.results[i].isFinal) {
+                value += e.results[i][0].transcript;
+            }
+        }
+        if ( value != "") {
+            document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
+        }
+    }
+    recognition.start();
+    """))
+
+result = streamlit_bokeh_events(
+    stt_button,
+    events="GET_TEXT",
+    key="listen",
+    refresh_on_update=False,
+    override_height=75,
+    debounce_time=0)
+
+if result:
+    if "GET_TEXT" in result:
+        st.write(result.get("GET_TEXT"))
+        client1.on_publish = on_publish                            
+        client1.connect(broker,port)  
+        message =json.dumps({"Act1":result.get("GET_TEXT").strip()})
+        ret= client1.publish("estrellas_ctrl", message)
+
+    
+    try:
+        os.mkdir("temp")
+    except:
+        pass
 
 
